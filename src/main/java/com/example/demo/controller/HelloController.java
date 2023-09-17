743e2f181752136d018d8f72403e557ae49329e8
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2023-09-17 11:36:42 +0300

    Deleting extra files

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,19 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
-public class ErrorController {
-    @GetMapping("/error")
+public class HelloController {
 
-    String helloWorld(){
-        return "You've reached an error page";
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello world!";
     }
-
-
-
 }
